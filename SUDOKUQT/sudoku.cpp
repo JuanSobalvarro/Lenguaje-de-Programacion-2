15dfe643fec6bee95c16cb7b42aa2a13c920ea0f
JuanSobalvarro <sobalvarrog.juans@gmail.com>
2024-02-04 21:47:37 -0600

    No se hace el commit aaah

--- SUDOKUQT/sudoku.cpp
+++ SUDOKUQT/sudoku.cpp
@@ -1,28 +1,3 @@
 #include "sudoku.h"
 
-std::vector<std::vector<int>> sudoku::generateGrid(std::string difficulty)
-{
-    std::vector<std::vector<int>> grid(9);
-
-    for (auto i = 0; i < 9; i++)
-    {
-        std::vector<int> vec(9, 0);
-
-        grid[i] = vec;
-    }
-
-
-    return grid;
-}
-
-void sudoku::printGrid(std::vector<std::vector<int>> grid)
-{
-    for (auto vec: grid)
-    {
-        for (auto num: vec)
-        {
-            std::cout << num << " ";
-        }
-        std::cout << std::endl;
-    }
-}
+sudoku::sudoku() {}
